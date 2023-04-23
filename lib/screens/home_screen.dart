da948668834c912466c73865d7aac1a0da6bd1f2
Victor Silva <victor.silvaPC@hotmail.com>
2023-04-23 13:12:24 -0300

    feat: :sparkles: added the Battery Status Page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,26 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tesla_animated_app/constants.dart';
 import 'package:tesla_animated_app/home_controller.dart';
 
+import 'components/battery_status.dart';
 import 'components/door_lock.dart';
 import 'components/tesla_bottom_navigationbar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
   final HomeController _controller = HomeController();
+
+  late AnimationController _batteryAnimationController;
+  late Animation<double> _animationBattery;
+  late Animation<double> _animationBatteryStatus;
+
+  void setupBatteryAnimation() {
+    _batteryAnimationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 600),
+    );
+
+    _animationBattery = CurvedAnimation(
+      parent: _batteryAnimationController,
+      // Here the animation end on 0.5
+      // it ends on 300 miliseconds
+      curve: Interval(0.0, 0.5),
+    );
+
+    _animationBatteryStatus = CurvedAnimation(
+      parent: _batteryAnimationController,
+      // After a delay we start this animation
+      // after 60 milliseconds delay it start
+      // so it start at 360 end on 600 milliseconds
+      curve: Interval(0.6, 1),
+    );
+  }
+
+  @override
+  void initState() {
+    setupBatteryAnimation();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _batteryAnimationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
       // It rebuild the widget tree when any changes happend on our controller
       // This animation need listenable
-      animation: _controller,
+      animation: Listenable.merge([_controller, _batteryAnimationController]),
       builder: (context, _) {
         return Scaffold(
           bottomNavigationBar: TeslaBottomNavigationBar(
-            onTap: (value) {},
-            selectedTab: 0,
+            onTap: (index) {
+              if (index == 1)
+                _batteryAnimationController.forward();
+              else if (_controller.selectedBottomTab == 1 && index != 1)
+                _batteryAnimationController.reverse(from: 0.7);
+              _controller.onBottomNavigationTabChange(index);
+            },
+            selectedTab: _controller.selectedBottomTab,
           ),
           body: SafeArea(
             child: LayoutBuilder(
@@ -37,34 +89,80 @@ class HomeScreen extends StatelessWidget {
                         width: double.infinity,
                       ),
                     ),
-                    Positioned(
-                      top: constrains.maxHeight * 0.13,
-                      child: DoorLock(
-                        isLock: _controller.isBonnetLock,
-                        press: _controller.updateBonnetDoorLock,
+                    //Door Locks
+                    AnimatedPositioned(
+                      duration: defaultDuration,
+                      top: _controller.selectedBottomTab == 0
+                          ? constrains.maxHeight * 0.13
+                          : constrains.maxHeight / 2,
+                      child: AnimatedOpacity(
+                        duration: defaultDuration,
+                        opacity: _controller.selectedBottomTab == 0 ? 1 : 0,
+                        child: DoorLock(
+                          isLock: _controller.isBonnetLock,
+                          press: _controller.updateBonnetDoorLock,
+                        ),
+                      ),
+                    ),
+                    AnimatedPositioned(
+                      duration: defaultDuration,
+                      left: _controller.selectedBottomTab == 0
+                          ? constrains.maxWidth * 0.05
+                          : constrains.maxWidth / 2,
+                      child: AnimatedOpacity(
+                        duration: defaultDuration,
+                        opacity: _controller.selectedBottomTab == 0 ? 1 : 0,
+                        child: DoorLock(
+                          isLock: _controller.isLeftDoorLock,
+                          press: _controller.updateLeftDoorLock,
+                        ),
+                      ),
+                    ),
+                    AnimatedPositioned(
+                      duration: defaultDuration,
+                      right: _controller.selectedBottomTab == 0
+                          ? constrains.maxWidth * 0.05
+                          : constrains.maxWidth / 2,
+                      child: AnimatedOpacity(
+                        duration: defaultDuration,
+                        opacity: _controller.selectedBottomTab == 0 ? 1 : 0,
+                        child: DoorLock(
+                          isLock: _controller.isRightDoorLock,
+                          press: _controller.updateRightDoorLock,
+                        ),
+                      ),
+                    ),
+                    AnimatedPositioned(
+                      duration: defaultDuration,
+                      bottom: _controller.selectedBottomTab == 0
+                          ? constrains.maxHeight * 0.17
+                          : constrains.maxHeight / 2,
+                      child: AnimatedOpacity(
+                        duration: defaultDuration,
+                        opacity: _controller.selectedBottomTab == 0 ? 1 : 0,
+                        child: DoorLock(
+                          isLock: _controller.isTrunkLock,
+                          press: _controller.updateTrunkDoorLock,
+                        ),
+                      ),
+                    ),
+                    // Battery
+                    Opacity(
+                      opacity: _animationBattery.value,
+                      child: SvgPicture.asset(
+                        "assets/icons/Battery.svg",
+                        width: constrains.maxWidth * 0.45,
                       ),
                     ),
                     Positioned(
-                      left: constrains.maxWidth * 0.05,
-                      child: DoorLock(
-                        isLock: _controller.isLeftDoorLock,
-                        press: _controller.updateLeftDoorLock,
+                      top: 50 * (1 - _animationBatteryStatus.value),
+                      height: constrains.maxHeight,
+                      width: constrains.maxWidth,
+                      child: Opacity(
+                        opacity: _animationBatteryStatus.value,
+                        child: BatteryStatus(constrains: constrains),
                       ),
-                    ),
-                    Positioned(
-                      right: constrains.maxWidth * 0.05,
-                      child: DoorLock(
-                        isLock: _controller.isRightDoorLock,
-                        press: _controller.updateRightDoorLock,
-                      ),
-                    ),
-                    Positioned(
-                      bottom: constrains.maxHeight * 0.17,
-                      child: DoorLock(
-                        isLock: _controller.isTrunkLock,
-                        press: _controller.updateTrunkDoorLock,
-                      ),
-                    ),
+                    )
                   ],
                 );
               },
